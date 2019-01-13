a6b34d8b0cca2b2fba67803118b3536015f55587
sanfordwinslow <46580829+sanfordwinslow@users.noreply.github.com>
2019-01-12 16:44:01 -0800

    Update timestamp.js

--- timestamp.js
+++ timestamp.js
@@ -1,9 +1,4 @@
 function dateTime(){
-  // var currentdate = new Date(); 
-  var datetime = "Last Sync: " + currentdate.getDate() + "/"
-                + (currentdate.getMonth()+1)  + "/" 
-                + currentdate.getFullYear() + " @ "  
-                + currentdate.getHours() + ":"  
-                + currentdate.getMinutes() + ":" 
-                + currentdate.getSeconds();
+  var my_time = new Date();
+  alert(my_time); 
 }
